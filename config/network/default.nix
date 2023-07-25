38daf21297e7afd4d82fbd4a20bfec6ea40378db
Lilian Schall <lilian.schall@epita.fr>
2023-07-25 19:51:09 +0200

    feat: modified the network configuration so that it supports a dynamic configuration without static address attributed

--- config/network/default.nix
+++ config/network/default.nix
@@ -1,10 +1,25 @@
 {pkgs, config, ...}:
 let
-  secrets = import ../secrets;
+  secrets = import ../secrets.nix;
+  networkmanager.enable = true;
+  hostname = secrets.hostName;
 in
-{
-  networking = {
-    networkmanager.enable = true;
-    hostName = secrets.hostName;
+  {
+    networking = if secrets.network.hasStaticAddress then {
+      inherit networkmanager hostname;
+
+      defaultGateway = secrets.network.defaultGateway;
+      nameservers = [ "8.8.8.8" ];
+      interfaces.eth0 = {
+        ipAddress = secrets.network.ipAddress;
+        prefixLength = secrets.network.prefixLength;
+      };
+    # firewall = {
+    #   allowedTCPPorts = [];
+    #   allowedUDPPorts = [];
+    # }
+  }
+  else {
+    inherit networkmanager hostname;
   };
 }
