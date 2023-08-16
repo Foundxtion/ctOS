295a56bcf17fdc464a6b5cb1543f3664625533f9
Lilian Schall <lilian.schall@epita.fr>
2023-08-16 02:29:09 +0200

    fix: added non normal home-manager user

--- config/users/homeManager.nix
+++ config/users/homeManager.nix
@@ -1,5 +1,10 @@
 {config, pkgs, ...} :
 {
+  users.users.home-manager = {
+    isNormalUser = false;
+    description = "home-manager user";
+  };
+
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
