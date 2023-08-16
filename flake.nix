a95caad533cb5a533aa2bbb5e02b68b335deb01d
Lilian Schall <lilian.schall@epita.fr>
2023-08-16 02:26:19 +0200

    fix: typo in flake.nix

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follow = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
