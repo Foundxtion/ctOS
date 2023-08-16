4a604e5bd0f37a02b97304b7c9129ec86cdee4bc
Lilian Schall <lilian.schall@epita.fr>
2023-08-16 02:40:12 +0200

    fix: removed home-manager user

--- config/users/homeManager.nix
+++ config/users/homeManager.nix
@@ -1,12 +1,5 @@
 {config, pkgs, ...} :
 {
-  users.users.home-manager = {
-    isNormalUser = false;
-    isSystemUser = true;
-    group = "home-manager";
-    description = "home-manager user";
-  };
-
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
