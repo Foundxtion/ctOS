59aadd66c5585f04b930b1cc4840ec28d3c254fa
Lilian Schall <lilian.schall@epita.fr>
2023-08-17 02:20:19 +0200

    fix: fixed typo in ddclient config

--- config/services/default.nix
+++ config/services/default.nix
@@ -19,11 +19,11 @@ in
   services.ddclient = {
     enable = secrets.services.ddclient.enable;
     configFile = pkgs.writeText "ddclient-config-file" ''
-        use=web , web=${secrets.services.ddclient.web}
+        use=web, web=${secrets.services.ddclient.web}
         protocol=${secrets.services.ddclient.protocol}
-        server= ${secrets.services.ddclient.server}
-        login= ${secrets.services.ddclient.login}
-        password= ${secrets.services.ddclient.password}
+        server=${secrets.services.ddclient.server}
+        login=${secrets.services.ddclient.login}
+        password=${secrets.services.ddclient.password}
         @
     '';
   };
