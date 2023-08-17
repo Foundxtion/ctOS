bdea41d563eaedab0b2b799a091e9908062a857a
Lilian Schall <lilian.schall@epita.fr>
2023-08-17 18:09:27 +0200

    feat: trying to replace nixos-generate-config by a local test file for hardware-configuration

--- tests/config-test.sh
+++ tests/config-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cp iso/secrets-template.nix config/secrets.nix;
-nixos-generate-config --show-hardware-config > config/hardware-configuration.nix;
+cp tests/hardware-configuration.nix config;
 nix-build '<nixpkgs/nixos>' -I nixos-config=config/configuration.nix --dry-run;
 
 rm config/secrets.nix config/hardware-configuration.nix;
