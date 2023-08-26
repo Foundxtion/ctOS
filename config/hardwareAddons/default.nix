fd80ddd98a9be6714725d8bd0b01c0bb9338891d
Lilian Schall <lilian.schall@epita.fr>
2023-08-26 23:30:02 +0200

    feat: added nvidia driver activation

--- config/hardwareAddons/default.nix
+++ config/hardwareAddons/default.nix
@@ -1,11 +1,21 @@
 { config, pkgs, ... }:
 {
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
-
-    nvidiaSettings = true;
-
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      open = false;
+  
+      nvidiaSettings = true;
+  
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
 }
