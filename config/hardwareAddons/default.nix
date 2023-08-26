45f71aff7f17fbca07956c1b69949d0397f1537c
Lilian Schall <lilian.schall@epita.fr>
2023-08-26 23:43:31 +0200

    config: disabled specification of package for nvidia driver

--- config/hardwareAddons/default.nix
+++ config/hardwareAddons/default.nix
@@ -12,7 +12,7 @@
   
       nvidiaSettings = true;
   
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      # package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 
