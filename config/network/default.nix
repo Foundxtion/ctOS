f1a179384376a490e7b9131914923c003e586978
Lilian Schall <lilian.schall@epita.fr>
2023-09-10 21:18:40 +0200

    feat: added new firewall port rule

--- config/network/default.nix
+++ config/network/default.nix
@@ -15,8 +15,8 @@ in
         prefixLength = secrets.network.prefixLength;
       }];
     firewall = {
-        allowedTCPPorts = [22 80 88 389 443 464 636 749];
-        allowedUDPPorts = [22 80 88 389 443 464 636 749];
+        allowedTCPPorts = [22 80 88 389 443 464 636 749 8022];
+        allowedUDPPorts = [22 80 88 389 443 464 636 749 8022];
     };
   }
   else {
