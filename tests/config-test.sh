abc06ca8e690728b096daaaca9a35c22ca2737f8
Lilian Schall <lilian.schall@epita.fr>
2023-09-22 23:12:07 +0200

    feat: modified tests for new architecture

--- tests/config-test.sh
+++ tests/config-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cp iso/secrets-template.nix config/secrets.nix;
-cp tests/hardware-configuration.nix config;
-nix-build '<nixpkgs/nixos>' -I nixos-config=config/configuration.nix --dry-run && echo "Test passed !";
+cp settings/serverSettings.nix settings/settings.nix
+cp tests/hardware-configuration.nix entrypoint;
+nix-build '<nixpkgs/nixos>' -I nixos-config=entrypoint/configuration.nix --dry-run && echo "Test passed !";
 
-rm config/secrets.nix config/hardware-configuration.nix;
+rm settings/settings.nix entrypoint/hardware-configuration.nix;
 
