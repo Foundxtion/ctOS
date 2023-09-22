20d11031c5eb30053b6a1010355cb293c48209f4
Lilian Schall <lilian.schall@epita.fr>
2023-09-22 23:11:31 +0200

    feat: added secret settings

---
+++ settings/isoSettings.nix
@@ -0,0 +1,27 @@
+# This is the iso-settings.nix template !
+# Comment out the configuration you might use
+
+{ pkgs, config, ...}:
+{
+    imports = [ ../installer ];
+    fndx.networking = {
+        # Enable / disable Network IP Attribution... (default: true)
+        useDHCP = false;
+        # ... Or (advanced) use static address !
+        ipv4Address = "255.255.255.255";
+        prefixLength = 0;
+        defaultGateway = "255.255.255.255";
+    };
+
+    installer = {
+        # What git remote to use as your Foundxtion configuration ?
+        git-remote = "https://github.com/foundxtion/foundxtion";
+
+        # Are you installing a server ?
+        # If yes, you may want to enable ssh into root
+        ssh = {
+            enable = true;
+            usedRootKey = "";
+        };
+    };
+}
---
+++ settings/serverSettings.nix
@@ -0,0 +1,42 @@
+# This is the serverSettings.nix template !
+
+{ pkgs, config, ...}:
+let
+    domain = "example.com";
+in
+{
+    fndx.user.name = "webServer";
+    # please use the following command to create your password hash
+    # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+    fndx.user.initialHashedPassword = "";
+    fndx.networking = {
+        useDHCP = false;
+        ipv4Address = "192.168.1.192";
+        prefixLength = 24;
+        defaultGateway = "192.168.1.1";
+    };
+
+    fndx.webServer = {
+        enable = true;
+        domain = domain;
+        acme-email = "user@example.com";
+        dynamicDns = {
+            enable = true;
+            protocol = "namecheap";
+            password = "password";
+        };
+        virtualHosts = {
+          # NixOS nginx configuration for now
+        };
+    };
+    fndx.services.mailserver = {
+        enable = true;
+        domain = domain;
+        loginAccounts = {};
+    };
+    fndx.services.openssh = {
+        enable = true;
+        authorizedKeys = [""];
+    };
+    fndx.services.docker.enable = true;
+}
