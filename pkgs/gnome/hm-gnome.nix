7154c4dfe8d92aee17b57696cd411aec83ea2a35
Lilian Schall <lilian.schall@epita.fr>
2023-10-06 21:12:11 +0200

    feat: unset wayland env variable

--- pkgs/gnome/hm-gnome.nix
+++ pkgs/gnome/hm-gnome.nix
@@ -62,6 +62,7 @@ with lib;
 
     home.sessionVariables = mkIf cfg.enable {
         GTK_THEME = "WhiteSur-Dark";
+        WAYLAND_DISPLAY = "";
     };
 
     home.packages = mkIf cfg.enable (with pkgs; [
