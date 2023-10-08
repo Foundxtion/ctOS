5d29d1414e68708bcccb9f8a62120d4fbac56f97
Lilian Schall <lilian.schall@epita.fr>
2023-10-08 20:15:54 +0200

    feat: trying to make janus integration work

--- modules/janus/default.nix
+++ modules/janus/default.nix
@@ -2,6 +2,7 @@
 let
     cfg = config.fndx.janus;
     dnBuilder = with lib.strings; (server_name: concatMapStringsSep "," (x: "dc=" + x) (splitString "." (toLower server_name)));
+    server = "ldap://ldap.${lib.strings.toLower cfg.realm}";
 in
 with lib;
 {
@@ -24,12 +25,39 @@ with lib;
     config = mkIf cfg.enable {
         fndx.authentication.ldap = {
             enable = true;
-            server = "ldap://ldap.${lib.strings.toLower cfg.realm}";
+            server = server;
             dn = dnBuilder cfg.realm;
         };
 	fndx.authentication.krb5 = {
 	    enable = true;
 	    realm = cfg.realm;
 	};
+
+        services.sssd = {
+            enable = true;
+            config = ''
+                [sssd]
+                config_file_version = 2
+                domains = ${lib.strings.toLower cfg.realm}
+
+                [nss]
+                override_shell = ${config.users.defaultUserShell}/bin/zsh
+
+                [domain/${lib.strings.toLower cfg.realm}]
+                id_provider = ldap
+                auth_provider = ldap
+
+                ldap_uri = ${server}
+                ldap_search_base = ${cfg.dn}
+		auth_provider = krb5;
+		krb5_server = ${lib.strings.toLower cfg.realm}
+		krb5_kpasswd = ${lib.strings.toLower cfg.realm}
+		krb5_realm = ${lib.strings.toUpper cfg.realm}
+
+                entry_cache_timeout = 600
+                ldap_network_timeout = 2
+		cache_credentials = True
+            '';
+        };
     };
 }
--- modules/janus/ldap.nix
+++ modules/janus/ldap.nix
@@ -13,7 +13,6 @@ with lib;
                 type = types.str;
                 description = mdDoc "Janus LDAP server url";
             };
-
             dn = mkOption {
                 example = "dc=example,dc=org"; 
                 type = types.str;
@@ -23,38 +22,6 @@ with lib;
     };
 
     config = mkIf cfg.enable {
-
-        services.sssd = {
-            enable = true;
-            config = ''
-                [sssd]
-                config_file_version = 2
-                services = nss, pam, ssh
-                domains = LDAP
-
-                [nss]
-                override_shell = ${config.users.defaultUserShell}/bin/zsh
-
-                [domain/LDAP]
-                cache_credentials = true
-                enumerate = false
-
-                id_provider = ldap
-                auth_provider = ldap
-
-                ldap_uri = ${cfg.server}
-                ldap_search_base = ${cfg.dn}
-                ldap_user_search_base = ou=users,${cfg.dn}?subtree?(objectClass=posixAccount)
-                ldap_group_search_base = ou=groups,${cfg.dn}?subtree?(objectclass=posixGroup)
-                ldap_id_use_start_tls = true
-                ldap_schema = rfc2307bis
-                ldap_user_gecos = cn
-
-                entry_cache_timeout = 600
-                ldap_network_timeout = 2
-            '';
-        };
-
 	environment.systemPackages = with pkgs; [
 	    openldap
 	];
