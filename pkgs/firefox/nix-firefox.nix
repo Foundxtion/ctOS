cd03d2242bffebd903d2bbe7921b10fe685293db
Lilian Schall <lilian.schall@epita.fr>
2024-04-15 20:08:19 +0200

    fix: used the maintained way of defining preferences in firefox

--- pkgs/firefox/nix-firefox.nix
+++ pkgs/firefox/nix-firefox.nix
@@ -7,15 +7,15 @@ with lib;
     options.fndx.packages.firefox.enable = mkEnableOption "Firefox for Foundxtion";
 
     config = mkIf cfg.enable {
-        environment.systemPackages = with pkgs; optionals (config.fndx.netauth.enable) [
-            (wrapFirefox firefox-unwrapped {
-                extraPrefs = ''
-                    pref("network.negotiate-auth.trusted-uris", "${toLower config.fndx.netauth.realm},.${toLower config.fndx.netauth.realm}");
-                    pref("network.trr.excluded-domains", "${toLower config.fndx.netauth.realm}");
-                '';
-            })
-        ] ++ optionals (!config.fndx.netauth.enable) [
-            firefox
-        ];
+        programs.firefox = {
+            enable = true;
+            preferences = mkIf config.fndx.netauth.enable {
+                "network.negotiate-auth.trusted-uris" = "${strings.toLower config.fndx.netauth.realm},.${strings.toLower config.fndx.netauth.realm}";
+                "network.trr.excluded-domains" = "${strings.toLower config.fndx.netauth.realm}";
+            };
+            policies = {
+                DisableTelemetry = true;
+            };
+        };
     };
 }
