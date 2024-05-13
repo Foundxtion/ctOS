5e047f01a763169c198e149d62f876d878cf51a6
Lilian Schall <lilian.schall@epita.fr>
2024-05-13 14:37:41 +0200

    fix: nautilus is now at a correct scale for screens with a high dpi

--- pkgs/nautilus/nix-nautilus.nix
+++ pkgs/nautilus/nix-nautilus.nix
@@ -7,10 +7,31 @@ with lib;
     options.fndx.packages.nautilus.enable = mkEnableOption "Nautilus for Foundxtion";
 
     config = mkIf cfg.enable {
-        environment.systemPackages = with pkgs; [
-            gnome3.nautilus
+        environment.systemPackages = let
+            wrapped = pkgs.writeShellScriptBin "nautilus" ''
+                export GDK_SCALE=2;
+                exec ${pkgs.gnome3.nautilus}/bin/nautilus
+            '';
+        in
+        with pkgs; [
+            (symlinkJoin {
+                name = "nautilus";
+                paths = [
+                    wrapped
+                    gnome3.nautilus
+                ];
+            })
         ];
 
         services.gvfs.enable = mkIf config.fndx.packages.i3.enable true;
+        xdg.mime = mkIf config.fndx.packages.i3.enable {
+            addedAssociations = {
+                # This shouldn't be necessary, but just for good measure...
+                "inode/directory" = "org.gnome.Nautilus.desktop";
+            };
+            defaultApplications = {
+                "inode/directory" = "org.gnome.Nautilus.desktop";
+            };
+        };
     };
 }
