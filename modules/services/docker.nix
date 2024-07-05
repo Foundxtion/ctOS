489fc520e3794d31dabb29e0acba98099b131baa
Lilian Schall <lilian.schall@epita.fr>
2024-07-05 18:24:38 +0200

    fix: typo

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -8,6 +8,6 @@ with lib;
   config = mkIf config.fndx.services.docker.enable {
       virtualisation.docker.enable = true; 
       virtualisation.docker.package = pkgs.docker_25;
-      hardware.nvdia-container-toolkit.enable = config.fndx.hardware.nvidia.enable;
+      hardware.nvidia-container-toolkit.enable = config.fndx.hardware.nvidia.enable;
   };
 }
