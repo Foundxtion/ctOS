437c0e04ec7693b5b841d42f0d0e96e30129d196
Lilian Schall <lilian.schall@epita.fr>
2024-12-26 20:00:01 +0100

    feat: adding package extension

--- pkgs/vscode/hm-vscode.nix
+++ pkgs/vscode/hm-vscode.nix
@@ -3,5 +3,29 @@ with lib;
 {
     programs.vscode = mkIf osConfig.fndx.packages.vscode.enable {
         enable = true;
+        extensions = with pkgs.vscode-extensions; [
+            vscodevim.vim
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+                name = "jetbrains-rider-dark-theme";
+                publisher = "EdwinSulaiman";
+                version = "1.0.2";
+                sha256 = "";
+            }
+
+            {
+                name = "vscode-jetbrains-rider-icon-theme";
+                publisher = "chadalen";
+                version = "2.24.0";
+                sha256 = "";
+            }
+
+            {
+                name = "ms-vscode";
+                publisher = "vscode-typescript-next";
+                version = "5.8.20241225";
+                sha256 = "";
+            }
+        ];
     };
 }
