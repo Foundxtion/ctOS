849c1fb8bb537d773108486741ea1c4ce93269f0
Lilian 'S3l4h' Schall <68446910+LilianSchall@users.noreply.github.com>
2024-12-26 21:06:58 +0100

    Feat/VSCode Standardization (#13)

--- pkgs/vscode/hm-vscode.nix
+++ pkgs/vscode/hm-vscode.nix
@@ -3,5 +3,50 @@ with lib;
 {
     programs.vscode = mkIf osConfig.fndx.packages.vscode.enable {
         enable = true;
+        enableExtensionUpdateCheck = false;
+        enableUpdateCheck = false;
+        userSettings = {
+            "window.titleBarStyle" = "custom";
+            "[typescriptreact]" = {
+                "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[css]" = {
+                "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "[json]" = {
+                "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "editor.fontSize" = 13;
+            "workbench.iconTheme" = "vscode-jetbrains-icon-theme-2023-auto";
+            "editor.fontFamily" = "'JetBrains mono'";
+            "[typescript]" = {
+                "editor.defaultFormatter" = "esbenp.prettier-vscode";
+            };
+            "workbench.colorTheme" = "JetBrains Rider Dark Theme";
+        };
+            extensions = with pkgs.vscode-extensions; [
+            vscodevim.vim
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+                name = "jetbrains-rider-dark-theme";
+                publisher = "EdwinSulaiman";
+                version = "1.0.2";
+                sha256 = "UkcP8fuIHE+mZ7bnVuux17gnd7XzLmBAnvPGeE1ptCs=";
+            }
+
+            {
+                name = "vscode-jetbrains-icon-theme";
+                publisher = "chadalen";
+                version = "2.24.0";
+                sha256 = "YdzJmZ9dfu71FDCYnrseX2ago+WGPU7f8kf6uZgI8rY=";
+            }
+
+            {
+                name = "vscode-typescript-next";
+                publisher = "ms-vscode";
+                version = "5.8.20241225";
+                sha256 = "1ojlDsWnpwvXMVEQeKN/RlNNVZh6pT3n/mKHD+hTqcI=";
+            }
+        ];
     };
 }
