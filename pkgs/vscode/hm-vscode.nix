74aad4f2c63b687a636689d94d6a536e540262c9
Lilian Schall <lilian.schall@epita.fr>
2024-12-26 20:37:10 +0100

    fix: fixed typo in extension name

--- pkgs/vscode/hm-vscode.nix
+++ pkgs/vscode/hm-vscode.nix
@@ -10,21 +10,18 @@ with lib;
                 name = "jetbrains-rider-dark-theme";
                 publisher = "EdwinSulaiman";
                 version = "1.0.2";
-                sha256 = "";
             }
 
             {
-                name = "vscode-jetbrains-rider-icon-theme";
+                name = "vscode-jetbrains-icon-theme";
                 publisher = "chadalen";
                 version = "2.24.0";
-                sha256 = "";
             }
 
             {
                 name = "ms-vscode";
                 publisher = "vscode-typescript-next";
                 version = "5.8.20241225";
-                sha256 = "";
             }
         ];
     };
