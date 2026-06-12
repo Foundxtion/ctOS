fced753c2fe71d11854fc17dcc3fccadf2b36acd
Lilian Schall <lilian.schall@epita.fr>
2026-06-12 22:09:40 +0200

    feat: upgrade home-manager to 26.05

--- pkgs/homeManager.nix
+++ pkgs/homeManager.nix
@@ -1,17 +1,19 @@
 {pkgs, config, ...}:
 {
-  home.stateVersion = "25.11";
+  home.stateVersion = "26.05";
 
   imports = [
     ./alacritty/hm-alacritty.nix
     ./csharpkit/hm-csharpkit.nix
     ./discord/hm-discord.nix
     ./gtk/hm-gtk.nix
+    ./hyprland/hm-hyprland.nix
     ./i3/hm-i3.nix
     ./polybar/hm-polybar.nix
     ./rofi/hm-rofi.nix
     ./vim/hm-vim.nix
     ./vscode/hm-vscode.nix
+    ./waybar/hm-waybar.nix
     ./zsh/hm-zsh.nix
   ];
 }
